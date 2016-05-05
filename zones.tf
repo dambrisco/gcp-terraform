c5a0fd664879a43410729e18b15f977434704268
jwaldrip <jason@waldrip.net>
2016-05-04 18:13:06 -0600

    one zone

--- zones.tf
+++ zones.tf
@@ -1,11 +1,9 @@
 variable "zone-count" {
-  default = "3"
+  default = "1"
 }
 
 variable "zones" {
   default = {
     "zone-0" = "us-east1-b"
-    "zone-1" = "us-east1-c"
-    "zone-2" = "us-east1-d"
   }
 }
