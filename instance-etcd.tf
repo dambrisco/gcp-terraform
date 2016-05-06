0ba1608f776173fc11e32575a7e08cf91fc1a6a5
jwaldrip <jason@waldrip.net>
2016-05-06 10:19:23 -0600

    new resources + security

---
+++ instance-etcd.tf
@@ -0,0 +1,63 @@
+module "etcd-coreos-user-data" {
+  source                            = "git::https://github.com/brandfolder/terraform-coreos-user-data.git?ref=master"
+  etcd2_discovery                   = "${var.etcd_discovery_url}"
+  etcd2_advertise-client-urls       = "http://var!private_ipv4:2379,http://var!private_ipv4:4001"
+  etcd2_initial-advertise-peer-urls = "http://var!private_ipv4:2380,http://var!private_ipv4:7001"
+  etcd2_listen-client-urls          = "http://0.0.0.0:2379,http://0.0.0.0:4001"
+  etcd2_listen-peer-urls            = "http://var!private_ipv4:2380,http://var!private_ipv4:7001"
+  flannel_interface                 = "var!private_ipv4"
+  fleet_metadata                    = "role=etcd"
+  fleet_public_ip                   = "var!private_ipv4"
+  fleet_engine_reconcile_interval   = "10"
+  fleet_etcd_request_timeout        = "5.0"
+  fleet_agent_ttl                   = "120s"
+}
+
+resource "google_compute_disk" "etcd" {
+  count = "${var.etcd-count}"
+  name  = "etcd-${count.index}"
+  zone  = "${var.zone}"
+  image = "${var.etcd-image}"
+  type  = "pd-ssd"
+  size  = 100
+}
+
+resource "google_compute_global_address" "etcd" {
+  count = "${var.etcd-count}"
+  name  = "etcd-${count.index}"
+}
+
+resource "google_compute_instance" "etcd" {
+  count        = "${var.etcd-count}"
+  name         = "etcd-${count.index}"
+  description  = "Etcd master"
+  machine_type = "${var.etcd-instance-type}"
+  zone         = "${var.zone}"
+
+  tags = ["etcd"]
+
+  disk {
+    disk = "${element(google_compute_disk.etcd.*.self_link, count.index)}"
+  }
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+  }
+
+  network_interface {
+    network = "${google_compute_network.primary.name}"
+
+    access_config {
+      nat_ip = "${element(google_compute_global_address.etcd.*.address, count.index)}"
+    }
+  }
+
+  metadata {
+    user-data = "${module.etcd-coreos-user-data.user-data}"
+  }
+
+  service_account {
+    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+  }
+}
