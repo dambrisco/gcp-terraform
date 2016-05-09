0601952b5c8fb98211ffe5d401436cfe4188e611
jwaldrip <jason@waldrip.net>
2016-05-08 18:40:00 -0600

    subnetworks /24

--- network-primary.tf
+++ network-primary.tf
@@ -5,7 +5,7 @@ resource "google_compute_network" "primary" {
 resource "google_compute_subnetwork" "primary" {
   count         = "${length(split(",", var.zones))}"
   name          = "primary-us-${element(split(",", var.zones), count.index % length(split(",", var.zones)))}"
-  ip_cidr_range = "10.0.${count.index}.0/16"
+  ip_cidr_range = "10.0.${count.index}.0/24"
   network       = "${google_compute_network.primary.self_link}"
   region        = "${replace(element(split(",", var.zones), count.index % length(split(",", var.zones))), "/-[a-z]$/", "")}"
 }
